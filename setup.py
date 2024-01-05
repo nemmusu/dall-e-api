0f46e60cfe12bf6d08e6b194325f922a6bc83bb8
john00 <steri.cristian@gmail.com>
2024-01-05 17:46:45 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Specifica i moduli necessari
+include_modules = [
+    'PyQt5',
+    'openai',
+    'configparser',
+    'threading',
+    'requests',
+    'PyQt5.QtWidgets',
+    'PyQt5.QtGui',
+    'PyQt5.QtCore',
+]
+
+# Crea l'eseguibile
+executables = [Executable('dall-e_ui.pyw', base="Win32GUI")]
+
+# Configurazione per la build
+build_options = {
+    'includes': include_modules,
+    'include_files': [
+        ('config.ini', 'config.ini'),  # Includi il file di configurazione
+        # Aggiungi eventuali altri file necessari
+    ],
+}
+
+# Impostazioni per la build
+setup(
+    name='Dall-e API interface',
+    version='1.0',
+    description='API Interface for Dall-e',
+    options={'build_exe': build_options},
+    executables=executables
+)
